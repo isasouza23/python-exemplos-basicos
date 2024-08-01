fff59cf90271756f80e974200e5dcbb1d257ea6f
isasouza23 <isabellesouza659@gmail.com>
2024-08-01 15:47:13 -0300

    .

---
+++ 07_replace.py
@@ -0,0 +1,8 @@
+# Variavel
+mensagem = "Eu gosto de Jeep!"
+
+# Nova variavel que recebe o método replace
+nova_mensagem = mensagem.replace ("Jeep","BMW")
+
+# Exibir 
+print(nova_mensagem)
---
+++ 14_operador_and.py
@@ -0,0 +1,9 @@
+idade_livia = 18
+idade_matheus = 19
+
+# Operador AND (e)
+if idade_livia >=18 and idade_matheus >=18:
+    print("Pelo menos um dos dois e maior de idade.")
+else:
+    # Os 2 devem ser menores para atender esta condição
+    print("Livia e Matheus não são maiores de idade ")
